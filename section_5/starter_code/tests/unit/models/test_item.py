db373354482e242ced5a0a3e00ab5ab6376891c0
Andrius Bai <abaikstys@gmail.com>
2021-09-16 17:20:26 +0300

    78_unnitesting_RESTapi finished

---
+++ section_5/starter_code/tests/unit/models/test_item.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+from section_5.starter_code.models.item import ItemModel
+
+
+class ItemTest(TestCase):
+    def test_create_item(self):
+        i = ItemModel('Test', 19.56)
+
+        self.assertEqual('Test', i.name,
+                         'The item name after creation do not represent constructor argument.')
+        self.assertEqual(19.56, i.price,
+                         'The item name after creation do not represent constructor argument.')
+
+    def test_item_json(self):
+        i = ItemModel('Test', 19.56)
+        expected = {'name': 'Test', 'price': 19.56}
+
+        self.assertEqual(expected, i.json(),
+                         'The item name after creation do not represent constructor argument.')
