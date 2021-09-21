4639c672437e145d56a697cacadc8e0975bb5ff0
Andrius Bai <abaikstys@gmail.com>
2021-09-21 11:16:15 +0300

    a coment added to file unit_base_test.py

--- section_6/starter_code/tests/unit/unit_base_test.py
+++ section_6/starter_code/tests/unit/unit_base_test.py
@@ -1,4 +1,4 @@
-from section_6.starter_code.app import app
+from section_6.starter_code.app import app # This unused import is needed for loading StoreItem fos unittesting
 from unittest import TestCase
 
 
