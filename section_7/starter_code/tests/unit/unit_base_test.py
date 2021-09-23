6fafa944a429cd3429559487dbfcd744fc851159
Andrius Bai <abaikstys@gmail.com>
2021-09-23 14:46:48 +0300

    91_creating_user_model finished

---
+++ section_7/starter_code/tests/unit/unit_base_test.py
@@ -0,0 +1,6 @@
+from section_6.starter_code.app import app # This unused import is needed for loading StoreItem fos unittesting
+from unittest import TestCase
+
+
+class UnitBaseTest(TestCase):
+    pass
