753b123d723fe642cb0df0bb8d473bb63525acbb
Andrius Bai <abaikstys@gmail.com>
2021-09-24 12:03:29 +0300

    response = client.post('/register', data={'username': 'test_user', 'password': 'test_password'})

--- section_7/starter_code/tests/system/test_user.py
+++ section_7/starter_code/tests/system/test_user.py
@@ -8,23 +8,33 @@ class UserTest(BaseTest):
     def test_register_user(self):
         with self.app() as client:
             with self.app_context():
-                request = client.post('/register', data={'username': 'test_user', 'password': 'test_password'})
+                response = client.post('/register', data={'username': 'test_user', 'password': 'test_password'})
 
-                self.assertEqual(201, request.status_code, 'Request status code do not met expected.')
+                self.assertEqual(201, response.status_code, 'Request status code do not met expected.')
                 self.assertIsNotNone(UserModel.find_by_username('test_user'), "User was not saved to DB.")
-                self.assertEqual({'message': 'User was created successfully'},
-                                 json.loads(request.data),
-                                 "Request response message do not correspond to expected response message.")
-
-                request2 = client.post('/register', data={'username': 'test_user', 'password': 'test_password'})
-
-                self.assertEqual(400, request2.status_code, 'Request status code do not met expected.')
-                self.assertEqual({'message': 'A user already exists'},
-                                 json.loads(request2.data),
+                self.assertDictEqual({'message': 'User was created successfully'},
+                                 json.loads(response.data),
                                  "Request response message do not correspond to expected response message.")
 
     def test_register_login(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                client.post('/register', data={'username': 'test_user', 'password': 'test_password'})
+
+                auth_response = client.post('/auth',
+                                           data=json.dumps({'username': 'test_user', 'password': 'test_password'}),
+                                           headers={'Content-type': 'application/json'})
+
+                self.assertIn('access_token', json.loads(auth_response.data).keys())
 
     def test_register_duplicated_user(self):
-        pass
+        with self.app() as client:
+            with self.app_context():
+                client.post('/register', data={'username': 'test_user', 'password': 'test_password'})
+
+                response = client.post('/register', data={'username': 'test_user', 'password': 'test_password'})
+
+                self.assertEqual(400, response.status_code, 'Request status code do not met expected.')
+                self.assertDictEqual({'message': 'A user already exists'},
+                                 json.loads(response.data),
+                                 "Request response message do not correspond to expected response message.")
